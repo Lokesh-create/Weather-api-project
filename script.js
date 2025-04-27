a24f3274fa5c51554c590fdf98a7ac0f0827a4fb
Lokesh-create <lokeshsahu4685@gmail.com>
2025-04-27 13:59:20 +0530

    Final Commit

--- script.js
+++ script.js
@@ -47,24 +47,24 @@ async function checkWeather(city)
     switch(weather_data.weather[0].main)
     {
         case 'Clouds':
-            weather_img.src="cloud.png";
+            weather_img.src="images/cloud.png";
             break;
               //Agar weather ke main me Clouds likha hua hai to image update hogi
 
         case 'Clear':
-            weather_img.src='clear.png';
+            weather_img.src='images/clear.png';
             break;
 
         case 'Mist':
-            weather_img.src='mist.png';
+            weather_img.src='images/mist.png';
             break;
 
         case 'Rain':
-            weather_img.src='rain.png';
+            weather_img.src='images/rain.png';
             break;
 
         case 'Snow':
-            weather_img.src='snow.png';
+            weather_img.src='images/snow.png';
             break;
     }
 }   
